43e5cd1929d843a0d79b9ab14b4d37f052753ebc
Lian Wong <ltaylor.wong@gmail.com>
2015-02-23 08:38:59 -0500

    lab 2-23

--- Cows.java
+++ Cows.java
@@ -5,4 +5,13 @@ import java.io.*;
 public class Cows{
 
     private int R, C, E, N;
+
+    public Cows(){
+    }
+	
+
+	//-------[MAIN]---------------
+
+	public static void main(String args[]){
+	}
 }
