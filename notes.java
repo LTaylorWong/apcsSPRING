e05456972be912d289e903995c319e99857083ce
Lian Wong <ltaylor.wong@gmail.com>
2015-02-25 08:26:57 -0500

    notes 2-25

--- notes.java
+++ notes.java
@@ -1,6 +1,12 @@
-/*
+ /*
 system.out.printf()
             -prints on the same line
 	    -placeholders: %s (string) %d(int) %f(floting point number)
 	    -%n_s/d/f leaves n number of spaces for each string/int/float
+*/
+
+/*
+  mergesort(a.k.a. the stuyvesant way)
+         -sorts recursively
+	 -breaks apart the given set to sort and then merge
 */
