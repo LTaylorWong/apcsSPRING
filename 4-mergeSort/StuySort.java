715ba861b2d04e45a8806a21c713bba894bb450a
Lian Wong <ltaylor.wong@gmail.com>
2015-02-26 08:24:24 -0500

    Stuysorting

--- 4-mergeSort/StuySort.java
+++ 4-mergeSort/StuySort.java
@@ -6,7 +6,19 @@ public class StuySort{
     private ArrayList<Integer> A = new ArrayList<Integer>();
     private ArrayList<Integer> B = new ArrayList<Integer>();
 
-   
+
+    public StuySort(){
+	A.add(1);
+	A.add(3);
+	A.add(5);
+	A.add(7);
+	A.add(22);
+	B.add(2);
+	B.add(3);
+	B.add(7);
+	B.add(10);
+	B.add(22);
+    }
     public ArrayList<Integer> merge(ArrayList<Integer> A, ArrayList<Integer> B){
 	ArrayList<Integer> ai = new ArrayList<Integer>();
 	while (A.size()>0 && B.size()>0){
@@ -22,7 +34,7 @@ public class StuySort{
     //------------[Main]-----------
     public static void main(String args[]){
 	StuySort s = new StuySort();
-	System.out.println(ms.merge([1,3,5,7,22],[2,3,7,10,25]));
+	System.out.println(s.merge(s.A, s.B));
     }
     
 }
