2723b6874f74a53689b93b110b39af60deb872a7
Lian Wong <ltaylor.wong@gmail.com>
2015-03-04 08:36:12 -0500

    notes 3-4-15

--- notes.java
+++ notes.java
@@ -12,11 +12,16 @@ system.out.printf()
 */
 
 /*
-merge sort
-splits/levels logBASE2(1)
-each split loops thru array n
+  merge sort:2nlogN (aka nlogN)
+  - splits/levels logBASE2(1)
+  - each split loops thru array: n
+  - each levels of merge: n
+  linear search: n
+  binary search: logN
 
-linear search: n
-binary search: logN
+Big O notation
+   function f(n) is said to be O(g(n)) [order g(n) or Big O g(n) ]
+   if there exists some constant k such that K(g(n))> f(n) and the 
+   long term.
  
  */
