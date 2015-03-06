b1163bdb907b3e38c37e1e6460ab8800b1f05a0c
Liiiian <ltaylor.wong@gmail.com>
2015-03-06 00:21:27 -0500

    selecting

--- 5-Selection/Selection.java
+++ 5-Selection/Selection.java
@@ -1,2 +1,14 @@
-public class Selectoin
+public class Selection{
+    /*Methods:
+    sorting then finding index of
+    splitting bianrily
+    */
+    private int x;
+    public Selection(int k){
+            k=x;
+    }
+//-----[Main]-------
+public static void main(String[] args){
+    Selection s = new Selection();
+}
     }
