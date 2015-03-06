68c54e488088e58752afebe0e3888a462ee22f43
Liiiian <ltaylor.wong@gmail.com>
2015-03-06 00:27:09 -0500

    adding pivot creator

--- 5-Selection/Selection.java
+++ 5-Selection/Selection.java
@@ -1,11 +1,23 @@
+import java.util.Random;
+
 public class Selection{
     /*Methods:
     sorting then finding index of
     splitting bianrily
     */
-    private int x;
+    private int[] data;
+    private int p
+    public Selection(int[] stuff){
+            data=stuff;
+    }
     public Selection(int k){
-            k=x;
+        k=p;
+    }
+    
+    public int pivot(){
+            Random r = new Random();
+            int n = r.nextInt(data.length)+1;
+            p = n;
     }
 //-----[Main]-------
 public static void main(String[] args){
