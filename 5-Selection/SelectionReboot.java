4fb891998083ede305c7ad9ba84b3b7b5368e64b
Liiiian <ltaylor.wong@gmail.com>
2015-03-09 01:16:38 -0400

    Create SelectionReboot.java

---
+++ 5-Selection/SelectionReboot.java
@@ -0,0 +1,15 @@
+public SelectionReboot{
+   private int[] nums;
+
+    public SelectionReboot(int[] input){
+	nums = input;
+    }
+  
+  public int Select(int[] A, int k, int l, int h){
+  }
+  
+  //----------------[Main]-------------
+  public static void main(String[]args){
+    SelectionReboot sr = new SelectionReboot();
+  }
+}
