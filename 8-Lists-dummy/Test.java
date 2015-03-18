83bd352eb79dc4939a83edd36f5cedf842422a5f
Lian Wong <ltaylor.wong@gmail.com>
2015-03-18 08:38:40 -0400

    LL test

--- 8-Lists-dummy/Test.java
+++ 8-Lists-dummy/Test.java
@@ -10,15 +10,20 @@ public class Test{
 	long start, elapsed;
 
 	start= System.currentTimeMillis();
-	LinkedList<Integer> LL = new LinkedList<Integer>();
+        LinkedList<Integer> LL = new LinkedList<Integer>();
 	Random r = new Random();
 	int n = 10;
-	//adding to new LinkedList
-	for (int i=0;i<n;i++){
+	//adding to new LinkedList and summing it
+	for(int i=0;i<n;i++){
 	    int k=r.nextInt(10);
 	    LL.add(k);
 	}
 	System.out.println(LL);
+	int sumLL=0;
+	for(int j=0;j<LL.size();j++){
+	    sumLL=sumLL+LL.get(j);
+	}
+	System.out.println(sumLL);
 	//
 
 	elapsed= System.currentTimeMillis()-start;
