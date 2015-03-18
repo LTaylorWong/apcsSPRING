26ff3b018f7ac716d9c301e3b1c54e72692fef5d
Liiiian <ltaylor.wong@gmail.com>
2015-03-17 23:19:12 -0400

    LL Dummy

--- 8-Lists-dummy/LList.java
+++ 8-Lists-dummy/LList.java
@@ -1,20 +1,62 @@
-public class LList {
-		private Node l=null;
+public class LL {
+    
+    private Node start,end;
+    private int len;
+    
+    public LList() {
+	start = new Node("");
+	end = null;
+	start.setNext(end);
+	len = 0;
+    }
 
-		public void add(String s){
-				Node tmp = new Node(s);
-				tmp.setNext(l);
-				l = tmp;
-		}
+    public void add(String s){
+	Node temp = new Node(s);
+        start.setNext(temp);
+	temp.setNext(end);
+	end = temp;
+	len++;
+    }
 
-		public String toString(){
-				String s = "";
-				Node tmp;
-				for (tmp = l; tmp != null; tmp=tmp.getNext()){
-						s = s + tmp + " --> ";
-				}
-				s = s + "null";
-				return s;
-		}
-		
+    public Node get(int n) {
+	Node temp = start;
+	int counter = -1;
+	while(counter < n && temp != null) {
+	    temp = temp.getNext();
+	    counter++;
+	}
+	return tmp;
+    }
+
+    public void add(int n, String s) {
+	Node added = new Node(s);
+	Node before = get(n - 1);
+	System.out.println(before);
+	Node after = get(n);
+	System.out.println(after);
+	added.setNext(after);
+	before.setNext(added);	
+    }
+
+    public void remove(int n) {
+	Node before = get(n - 1);
+	Node after = get(n + 1);
+	before.setNext(after);
+	len--;
+    }
+    
+    public int getLength() {
+	return len;
+    }
+    
+    public String toString(){
+	String s = "null --> ";
+	Node temp;
+	for (tmp = start.getNext(); tmp != null; temp = temp.getNext()){
+	    s = s + temp + " --> ";
+	}
+	s = s + "null";
+	return s;
+    }
+    
 }
