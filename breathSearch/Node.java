1e5cab8613a89e540c7b022d529e1e07ccdc98e5
Lian Wong <ltaylor.wong@gmail.com>
2015-04-13 08:30:48 -0400

    bfs

--- breathSearch/Node.java
+++ breathSearch/Node.java
@@ -1,4 +1,23 @@
 import java.util.*;
 
 public class Node{
+    private int x,y;
+    private Node prev;
+
+    public Node(int x, int y){
+	this.x=x;
+	this.y=y;
+    }
+
+    public int getX{
+	return x;
+    }
+
+    public int getY{
+	return y;
+    }
+
+    public Node getPrev{
+	return prev;
+    }
 }
