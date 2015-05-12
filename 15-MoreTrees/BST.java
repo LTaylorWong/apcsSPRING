585afd50030332e16b73804e3b1e3be84084ef52
Lian Wong <ltaylor.wong@gmail.com>
2015-05-12 08:22:22 -0400

    tree things

--- 15-MoreTrees/BST.java
+++ 15-MoreTrees/BST.java
@@ -93,6 +93,15 @@ public class BST{
 	   return max(pl1,pl2,pl3);
 	}
     }
+    /* Tree:
+                3
+	    /       \
+	   7         12
+	  / \        / \
+       15     8     17 19
+       / \   / \    / 
+       16 22 13 21 30
+     */
     /* split dupes:
        parent and leaf have the same value--->
        inserts a new node between them that is one less than that value
