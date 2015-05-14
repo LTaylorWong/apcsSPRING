352187aadc3309263c6faf8259e5c9010fe1e16a
Lian Wong <ltaylor.wong@gmail.com>
2015-05-14 08:30:57 -0400

    heap

--- 16-Heap/Heap.java
+++ 16-Heap/Heap.java
@@ -9,30 +9,10 @@ public class Heap{
       16 22 13 21  30
      */
 
-    /*
-      Each Child is greater that the parent
-      each has full possible left and right
-      given node n, n'a children are 2n, 2n+1
-     */
-    /* Remove:
-      1.remove min
-      2.replace with lowest right value
-      3.pushdown: swap the root with the smaller childand continue down
-      until we're done
-     */
-    /* Insert:
-       1.add new lowest left node
-       2.sift up
-     */
-    
-    /* ~Runtimes~
-      find min: O(1)
-      --------------
-      remove min,
-      push down,
-      insert: O(log n)
-      ---------------
-      heapSort: O(nlogn)
-     */
+    public void minHeap{
+    }
+
+    public void maxHeap{
+    }
 }
 
